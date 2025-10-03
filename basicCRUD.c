ab00c532e7d03927db5d0a9dc973d35d1c7b5ad9
Jitendra Teli <gbetli7080.emal@gmail.com>
2025-10-03 19:40:10 +0530

    altered variable names as suggested, to be more understandable

--- basicCRUD.c
+++ basicCRUD.c
@@ -1,165 +1,165 @@
 #include<stdio.h>
 #include<string.h>
 
-void addUser(char *fileName,int id, char *userName,int age) {
-    FILE *p = fopen(fileName,"a");                            
+void addUser(char *fileName,int userId, char *userName,int userAge) {
+    FILE *filePointer = fopen(fileName,"a");                            
 
-    if(p == NULL) {
+    if(filePointer == NULL) {
         printf("Internal Server Error");
     }
 
-    fprintf(p,"%d,%s,%d\n",id,userName,age);
-    fclose(p);
+    fprintf(filePointer,"%d,%s,%d\n",userId,userName,userAge);
+    fclose(filePointer);
 
 }
 
 int getIDForNewUser(char *fileName) {                          
-    FILE *p = fopen(fileName, "r");
-    if(p == NULL) return 2000;
-    int id =  2000,tid = 2000,tage = 0;
+    FILE *filePointer = fopen(fileName, "r");
+    if(filePointer == NULL) return 2000;
+    int userId =  2000,tid = 2000,tage = 0;
     char tname[100];                                      
 
-    while(fscanf(p,"%d,%99[^,],%d",&tid,tname,&tage) == 3) {
-        if(id < tid) id = tid;
+    while(fscanf(filePointer,"%d,%99[^,],%d",&tid,tname,&tage) == 3) {
+        if(userId < tid) userId = tid;                                      // to get the max available user id for incremental sequence 
     }
 
-    fclose(p);
-    return id+1;
+    fclose(filePointer);
+    return userId+1;
     
 }
 
 void readUsers(char *fileName) {
-    FILE *p = fopen(fileName,"r");
+    FILE *filePointer = fopen(fileName,"r");
 
-    if(p == NULL) printf("Internal Server Error");
+    if(filePointer == NULL) printf("Internal Server Error");
     
 
-    int id,age;
-    char name[100];
+    int userId,userAge;
+    char userName[100];
 
     printf("\nID       Name                                Age\n");
     printf("________________________________________________\n\n");
 
-    while(fscanf(p,"%d,%99[^,],%d",&id,name,&age) == 3) {
-        printf("%-8d %-30s %8d\n",id,name,age);
+    while(fscanf(filePointer,"%d,%99[^,],%d",&userId,userName,&userAge) == 3) {
+        printf("%-8d %-30s %8d\n",userId,userName,userAge);
     }
 
-    fclose(p);
+    fclose(filePointer);
 }
 
-void updateUser(char *fileName,int id,char *userName,int age) {
-    FILE *p = fopen(fileName,"r");
+void updateUser(char *fileName,int userId,char *userName,int userAge) {
+    FILE *filePointer = fopen(fileName,"r");
 
-    if(p == NULL) printf("Internal Server Error");
+    if(filePointer == NULL) printf("Internal Server Error");
 
-    int found = 0;
+    int userFound = 0;                                 // 0 -> false , 1 -> True
 
 
 
     int tid,tage;
     char tname[100];
 
-    FILE *t = fopen("temp.txt","w");
+    FILE *temporaryFile = fopen("temp.txt","w");
 
-    while(fscanf(p,"%d,%99[^,],%d",&tid,tname,&tage) == 3) {
+    while(fscanf(filePointer,"%d,%99[^,],%d",&tid,tname,&tage) == 3) {
 
-        if(id == tid) {
-            fprintf(t,"%d,%s,%d\n",id,userName,age);
-            found = 1;
-        }else fprintf(t,"%d,%s,%d\n",tid,tname,tage);
+        if(userId == tid) {
+            fprintf(temporaryFile,"%d,%s,%d\n",userId,userName,userAge);
+            userFound = 1;
+        }else fprintf(temporaryFile,"%d,%s,%d\n",tid,tname,tage);
     }
 
-    fclose(p);
-    fclose(t);
+    fclose(filePointer);
+    fclose(temporaryFile);
     
-    if(found == 1) {
+    if(userFound == 1) {
         remove(fileName);
         rename("temp.txt",fileName);
 
     } else {
         remove("temp.txt");
-        printf("id not found ");
+        printf("given user Id not Found ");
     }
 
 
 
 }
 
-void deleteUser(char *fileName,int id) {
-    FILE *p = fopen(fileName,"r");
+void deleteUser(char *fileName,int userId) {
+    FILE *filePointer = fopen(fileName,"r");
 
-    if(p == NULL) printf("Internal Server Error");
+    if(filePointer == NULL) printf("Internal Server Error");
     
-    int found = 0;
+    int userFound = 0;
 
     int tid,tage;
     char tname[100];
 
-    FILE *t = fopen("temp.txt","w");
+    FILE *temporaryFile = fopen("temp.txt","w");
 
-    while(fscanf(p,"%d,%99[^,],%d",&tid,tname,&tage) == 3) {
+    while(fscanf(filePointer,"%d,%99[^,],%d",&tid,tname,&tage) == 3) {
 
-        if(id == tid) {
-            found = 1;
-        }else fprintf(t,"%d,%s,%d\n",tid,tname,tage);
+        if(userId == tid) {
+            userFound = 1;
+        }else fprintf(temporaryFile,"%d,%s,%d\n",tid,tname,tage);
     }
 
-    fclose(p);
-    fclose(t);
+    fclose(filePointer);
+    fclose(temporaryFile);
     
-    if(found == 1) {
+    if(userFound == 1) {
         remove(fileName);
         rename("temp.txt",fileName);
 
     } else {
         remove("temp.txt");
-        printf("id not found ");
+        printf("given user Id not Found ");
     }
 
 }
 void createUser(char *fileName) {
     
-    int tage;
-    char tname[100];
+    int userAge;
+    char userName[100];
 
-    printf("Enter user name : ");
-    scanf(" %99[^\n]",tname); 
-    printf("Enter user age  : ");
-    scanf("%d",&tage);
+    printf("Enter user userName : ");
+    scanf(" %99[^\n]",userName); 
+    printf("Enter user userAge  : ");
+    scanf("%d",&userAge);
 
-    addUser(fileName,getIDForNewUser(fileName),tname,tage);
+    addUser(fileName,getIDForNewUser(fileName),userName,userAge);
 
 }
 
 
 void editUser(char *fileName) {
-    int id,age;
-    char name[100];
+    int userId,userAge;
+    char userName[100];
 
-    printf("Enter user ID   : ");
-    scanf("%d",&id);
-    printf("Enter user name : ");
-    scanf(" %99[^\n]",name);  
-    printf("Enter user age  : ");
-    scanf("%d",&age);
+    printf("Enter user userId   : ");
+    scanf("%d",&userId);
+    printf("Enter user userName : ");
+    scanf(" %99[^\n]",userName);  
+    printf("Enter user userAge  : ");
+    scanf("%d",&userAge);
 
-    updateUser(fileName,id,name,age);
+    updateUser(fileName,userId,userName,userAge);
 
 }
 
 void removeUser(char *fileName) {
-    int id;
+    int userId;
 
-    printf("Enter user ID   : ");
-    scanf("%d",&id);
+    printf("Enter user userId   : ");
+    scanf("%d",&userId);
 
-    deleteUser(fileName,id);
+    deleteUser(fileName,userId);
 }
 
-void meenu(char *fileName) {
-    int c = 0;
+void showMenu(char *fileName) {
+    int userChoice = 0;
     do{
-        c = 0;
+        userChoice = 0;
         
         printf("\n1. Create User\n");
         printf("2. Update User\n");
@@ -167,10 +167,10 @@ void meenu(char *fileName) {
         printf("4. Show Users\n");
         printf("0. Exit\n");
         printf("Enter operation : ");
-        scanf("%d",&c);
+        scanf("%d",&userChoice);
         printf("\n");
 
-        switch(c) {
+        switch(userChoice) {
             case 0: printf("\nClosing the app, adios amigo");
             break;
             case 1:createUser(fileName);
@@ -183,7 +183,7 @@ void meenu(char *fileName) {
             break;
             default: printf("Invalid option");
         }
-    } while(c);
+    } while(userChoice);
 }
 
 
@@ -192,7 +192,7 @@ int main() {
     
     char *fileName = "users.txt";
 
-    meenu(fileName);
+    showMenu(fileName);
 
     return 0;
 
