f52a7043de0fe87490a515d77ae3f98fed7d8144
Jitendra Teli <gbetli7080.emal@gmail.com>
2025-11-23 11:42:03 +0530

    update : modification in structur

--- cacheLRU.c
+++ cacheLRU.c
@@ -2,35 +2,86 @@
 
 typedef struct Node {
     int key;
-    char *token;
+
     struct Node *next;
     struct NOde *prev;
 } Node;
 
+typedef struct HashNode {
+    int key;
+    char* token;
+
+    struct HashNode *next;
+    struct HashNode *prev;
+    struct HashNode *child;
+
+} HashNode;
+
+
 Node *head = NULL;
 Node *tail = NULL;
 
+HashNode **HashMap = NULL;
 
-Node* generateMap(int capacity) {
-    Node* map = (Node *) calloc(capacity,sizeof(Node));
-
-
-    head = map;
-    tail = map + capacity - 1;
-
-    map[0].prev = NULL;
-    map[0].next = map + 1;
-
-    map[capacity-1].prev = map + capacity - 2;
-    map[capacity-1].next = NULL;
+void access(int key) {
     
+}
 
-    for(int i = 1; i<capacity-1; i++) {
-        map[i].next = map + i + 1;
-        map[i].prev = map + i - 1;
-    }
+int hash(int key) {
+
+    return key;
+}
+
+HashNode** generateMap(int capacity) {
+    HashNode **map = (HashNode **) calloc(capacity,sizeof(HashNode*));
+
+    for(int i = 0; i<capacity; i++) map[i] = NULL;
 
     return map;
+}
+
+
+
+void put(int key,char *token) {
+    int index = hash(key);
+
+    HashNode *temp = HashMap[index];
+
+    if(temp == NULL) {
+        HashMap[index] = (HashNode *) malloc(sizeof(HashNode));
+        HashMap[index]->key = key;
+        HashMap[index]->token = token;
+    } else {
+        while(temp->next != NULL) {
+            
+            if(temp->key == key) {
+                temp->token = token;
+                return ;
+            }
+
+            temp = temp->next;
+        }
+
+        temp->next = (HashNode *) malloc(sizeof(HashNode));
+        temp->next->key = key;
+        temp->next->token = token;
+    }
+}
+
+HashNode* get(int key) {
+    int index = hash(key);
+
+    HashNode *temp = HashMap[index];
+    
+    while(temp != NULL) {
+        if(temp->key == key) {
+            access(key);
+            break;
+        }
+        temp = temp->next;
+    }
+
+    return temp;
 }
 
 
