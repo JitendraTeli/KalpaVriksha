3f84211a2bc58baa9c7975f4f5e3612276e5442a
Jitendra Teli <119528581+JitendraTeli@users.noreply.github.com>
2025-12-24 01:34:47 +0530

    FCFS Scheduler  (#12)
    * initialized branch
    
    * Implemented Generic HashMap
    
    * implemented required DS and structs
    
    * added process execution and input
    
    * Completed all functionalities
    
    * Tested and Verified working
    
    * Update : memory management

---
+++ SchedulerFCFS/DataStructures.c
@@ -0,0 +1,172 @@
+
+#include "DataStructures.h"
+
+#include<stdio.h>
+#include<stdlib.h>
+
+#define FACTOR 7.5
+
+int hash(int key,int size) {
+    key = key ^ (key >> (sizeof(key)*4));
+    return (key & (size-1));
+}
+
+float sizeFactor(int capacity,int size) {
+    return (float) size / (float) capacity;
+}
+
+HashMap* makeMap(int size) {
+    HashMap *map = malloc(sizeof(HashMap));
+    map->map = (HashNode ** ) calloc(size , sizeof(HashNode *));
+    map->capacity = size;
+    map->size = 0;
+    return map;
+}
+
+void insert(HashNode **head,HashNode *node) {
+    node->next = NULL;
+    if(*head == NULL) {
+        *head = node;
+    } else {
+        HashNode *temp = *head;
+        while(temp && temp->next ) {
+            temp = temp->next;
+        }
+        temp->next = node;
+    }
+}
+
+void reshape(HashMap *Map) {   
+    HashNode **oldMap = Map->map; 
+    int oldCapacity = Map->capacity;
+
+    Map->map = calloc(2 * Map->capacity , sizeof(HashNode*));
+
+    for(int i = 0; i<Map->capacity; i++) {
+        HashNode *temp = oldMap[i];
+
+        while(temp) {
+            HashNode *node = temp;
+            temp = temp->next;
+
+            insert(Map->map + hash(node->key,2 * Map->capacity),node);  
+        }
+    }
+    Map->capacity *= 2;
+
+    free(oldMap);
+}
+
+void put(HashMap *Map,int key,void *val) {
+    int index = hash(key,Map->capacity);
+
+    HashNode *temp = Map->map[index];
+
+    while(temp) {
+        if(temp->key == key)  {
+            temp->value = val;
+            return;
+        }
+
+        temp = temp->next;
+    }
+
+    HashNode *newNode = malloc(sizeof(HashNode));
+    newNode->key = key;
+    newNode->value = val;
+    newNode->next = NULL;
+
+    newNode->next = Map->map[index];
+    Map->map[index] = newNode;
+
+    Map->size++;
+
+    if(sizeFactor(Map->capacity,Map->size) > FACTOR) reshape(Map);
+}
+
+void* pop(HashMap *Map,int key) {
+    int index = hash(key,Map->capacity);
+
+    HashNode *temp = Map->map[index];
+    HashNode *prev = NULL;
+    while(temp) {
+        if(temp->key == key) {
+            break;
+        }
+        prev = temp;
+        temp = temp->next;
+    }
+    
+    if(temp == NULL) return NULL;
+
+    if(prev) prev->next = temp->next; else Map->map[index] = temp->next;
+
+    void *value = temp->value;
+
+    free(temp);
+
+    Map->size--;
+
+    return value;
+}
+
+
+void* get(HashMap *Map,int key) {
+    HashNode *temp = Map->map[hash(key,Map->capacity)];
+
+    while(temp) {
+        if(temp->key == key) {
+            return temp->value;
+        }
+
+        temp = temp->next;
+    }
+
+    return NULL;
+}
+
+
+List* makeList() {
+    List *list = malloc(sizeof(List));
+
+    list->head = NULL;
+    list->tail = NULL;
+
+    return list;
+}
+
+void enque(List *queue,Node *newNode) {
+    if(queue->head == NULL) {
+        queue->head = queue->tail = newNode;
+    } else {
+        newNode->prev = queue->tail;
+        queue->tail->next = newNode;
+        queue->tail = newNode;
+    }
+}
+
+Node* deque(List *queue) {
+    if(queue->head == NULL) return NULL;
+
+    Node *temp = queue->head;
+    queue->head = queue->head->next;
+    if(queue->head) queue->head->prev = NULL; else queue->tail = NULL;
+
+    temp->next = NULL;
+    temp->prev = NULL;
+
+    return temp;
+}
+
+void detach(List *queue,Node *node) {
+    if(queue->head == NULL) return;
+
+    if(node == queue->head) queue->head = queue->head->next;
+    if(node == queue->tail) queue->tail = queue->tail->prev;
+
+    if(node->next) node->next->prev = node->prev;
+    if(node->prev) node->prev->next = node->next;
+
+    node->next = NULL;
+    node->prev = NULL;
+} 
---
+++ SchedulerFCFS/main.c
@@ -0,0 +1,278 @@
+#include "DataStructures.h"
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#define NAME_LENGTH 20
+#define BUFFER_LENGTH 1024
+
+typedef enum type {
+    wait,
+    kill,
+    ready,
+    stop,
+} Type;
+
+typedef struct Event {
+    Type event;
+    int id;
+    struct Event *next;
+} Event;
+
+HashMap *Events = NULL;
+
+void addEvent(Type event,int id,int time);
+
+typedef struct Process {
+    char *name;
+    int id;
+    int time;
+    int startIO;
+    int timeIO;
+
+    Type status;
+    int waitingTime;
+    int turnAroundTime;
+
+} Process;
+
+typedef struct Schedular {
+    HashMap *processes;
+    List *waiting;
+    List *ready;
+    List *terminated;
+} Schedular;
+
+Schedular *fcfs;
+
+Process *current = NULL;
+int clock = 0;
+
+void addProcess(char *name,int id,int time,int startIO,int timeIO);
+
+void commenceInput();
+
+void handle(Event *event);
+
+Process* getNextProcess();
+
+void beginExecution();
+
+void printDetails(Process *Process);
+
+void printHeader();
+
+void showOutput();
+
+int main() {
+    Events = makeMap(8);
+
+    Schedular FCFS = {makeMap(8),makeList(),makeList(),makeList()};
+    fcfs = &FCFS;
+
+    commenceInput();
+
+    beginExecution();
+
+    showOutput();
+
+    free(Events->map);
+    free(Events);
+
+    free(fcfs->processes->map);
+    free(fcfs->processes);
+    free(fcfs->ready);
+    free(fcfs->waiting);
+    free(fcfs->terminated);
+
+    return 0;
+}
+
+void addEvent(Type event,int id,int time) {
+
+    Event *newEvent = malloc(sizeof(Event));
+    newEvent->event = event;
+    newEvent->id = id;
+    newEvent->next = NULL;
+
+    Event *eventList = get(Events,time);
+
+    if(eventList == NULL) {
+        eventList = newEvent;
+    } else {
+        Event *temp = eventList;
+
+        while(temp->next) temp = temp->next;
+
+        temp->next = newEvent;
+    }
+
+    put(Events,time,eventList);
+}
+
+
+void addProcess(char *name,int id,int time,int startIO,int timeIO) {
+    Process *process = malloc(sizeof(Process));
+
+    process->id = id;
+    process->name = strdup(name);
+    process->time = time;
+    process->startIO = startIO;
+    process->timeIO = timeIO;
+
+    process->status = ready;
+    process->turnAroundTime = 0;
+    process->waitingTime = 0;
+
+    Node *node = malloc(sizeof(Node));
+
+    node->prev = node->next = NULL;
+    node->value = process;
+
+    put(fcfs->processes,process->id,node);
+    enque(fcfs->ready,node);
+
+}
+
+void commenceInput() {
+    char *buffer = malloc(sizeof(char) * BUFFER_LENGTH);
+
+    int id,time,startIO,timeIO;
+    char *name = malloc(NAME_LENGTH * sizeof(char));
+
+    while(fgets(buffer,BUFFER_LENGTH-1,stdin) != NULL) {
+        startIO = timeIO = -1;  //no io
+
+        int allocations = sscanf(buffer,"%19s %d %d %d %d",name,&id,&time,&startIO,&timeIO);
+
+        if(allocations == -1) break;
+
+        if(allocations < 3) continue;
+
+        if(strcmp(name,"kill") == 0) {
+            addEvent(kill,id,time);
+        } else {
+            addProcess(name,id,time,startIO,timeIO);
+        }
+
+    }   
+    free(name);
+    free(buffer);   
+}
+
+void handle(Event *event) {
+    if(event == NULL) return;
+    Event *temp;
+
+    while(event) {
+        temp = event;
+        
+        Node *processNode = get(fcfs->processes,event->id);
+
+        if(!processNode) continue;
+
+        Process *process = processNode->value;
+
+        if(current == processNode->value) current = NULL;
+        if(process->status != kill) {
+
+            if(process->status == wait) detach(fcfs->waiting,processNode);
+            else if(process->status == ready) detach(fcfs->ready,processNode);
+            
+            if(event->event == kill) {
+                process->status = kill;
+                enque(fcfs->terminated,processNode);
+    
+            } else if(event->event == ready) {
+                if(process->status == wait) process->startIO = -1;
+                process->status = ready;
+                enque(fcfs->ready,processNode);
+    
+            } else if(event->event == wait) {
+                process->status = wait;
+
+                addEvent(ready,process->id,clock + process->timeIO);
+                enque(fcfs->waiting,processNode);
+            } 
+        }
+        event = event->next;
+
+        free(temp);
+    }
+}
+
+Process* getNextProcess() {
+    Process* nextProcess = fcfs->ready->head ? deque(fcfs->ready)->value: NULL;
+
+    if(nextProcess) {
+        if(!nextProcess->waitingTime && nextProcess->startIO != -1) {
+            addEvent(wait,nextProcess->id,clock + nextProcess->startIO);
+        }
+
+        nextProcess->waitingTime = clock - nextProcess->turnAroundTime;
+
+    }
+
+    return nextProcess;
+}
+
+void beginExecution() {
+    while(fcfs->ready->head || fcfs->waiting->head || current) {
+
+        handle(pop(Events,clock));
+        
+        if(current && current->turnAroundTime == current->time) {
+            current->turnAroundTime += current->waitingTime;
+            enque(fcfs->terminated,pop(fcfs->processes,current->id));
+            current = NULL;
+        }
+
+        if(current == NULL) {
+            if(current = getNextProcess()) continue;
+        }
+
+        if(current) {
+            current->turnAroundTime++;
+        }
+        clock++;
+    }
+}
+
+void printDetails(Process *Process) {
+    printf("%-20s",Process->name);
+    printf(" %-5d",Process->id);
+    printf(" %-7s",Process->status == kill ? "Killed":"OK");
+    printf(" %-10d",Process->time);
+    printf(" %-10d",Process->timeIO);
+    printf(" %-15d",Process->turnAroundTime);
+    printf(" %-15d",Process->waitingTime);
+    printf("\n");
+
+    free(Process->name);
+    free(Process);
+}
+
+void printHeader() {
+    printf("%-20s","Name");
+    printf(" %-5s","ID");
+    printf(" %-7s","Status");
+    printf(" %-10s","Burst");
+    printf(" %-10s","IO");
+    printf(" %-15s","TurnAround");
+    printf(" %-15s","Waiting");
+    printf("\n");
+}
+
+void showOutput() {
+    Node *node;
+
+    printHeader();
+
+    while((node = deque(fcfs->terminated))) {
+        printDetails(node->value);
+
+        free(node);
+        node = NULL;
+    }
+}
